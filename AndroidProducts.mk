b2752fbb42ab5562e47316655e406fe91556420c
Cathryn Gorczany <122968115+Cathryn2003@users.noreply.github.com>
2024-02-10 15:46:29 +0545

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_selene.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_selene-user \
-    twrp_selene-userdebug \
-    twrp_selene-eng
+    omni_selene-user \
+    omni_selene-userdebug \
+    omni_selene-eng
